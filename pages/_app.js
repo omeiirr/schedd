70d5dff668c8466ffc66d03495a4e27c16ded416
Omeir <omeirf.02@gmail.com>
2021-12-25 02:42:58 +0530

    ⚙ Configure Head for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import { Store } from '../store';
+import Head from 'next/head';
 import '../styles/index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 function MyApp({ Component, pageProps }) {
   return (
     <Store>
+      <Head>
+        <title>Sched</title>
+        <meta name="description" content="An app to keep your university schedule on track" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
