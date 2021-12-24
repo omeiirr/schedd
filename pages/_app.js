67e4dd4451e018ab5c280e88aafb7da45b078afc
Omeir <omeirf.02@gmail.com>
2021-12-25 02:40:30 +0530

    ➕ Setup Tailwind CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Store } from '../store';
+import '../styles/index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 function MyApp({ Component, pageProps }) {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./pages/**/*.js']
+  },
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
