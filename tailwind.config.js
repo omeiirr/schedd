c9829e81679bceb95e4a74e9d971a5f203ada060
Omeir <omeirf.02@gmail.com>
2021-12-26 02:03:57 +0530

    ⚙ Tailwind config: add custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     content: ['./pages/**/*.js', './components/**/*.js']
   },
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        above: '2px -8px 20px -10px rgba(0,0,0,0.3)',
+        below: '2px 8px 20px -10px rgba(0,0,0,0.3)'
+      }
+    }
   },
   plugins: []
 };
