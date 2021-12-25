d33bef7b821f3e6fb8984959d3cb730163d2c151
Omeir <omeirf.02@gmail.com>
2021-12-26 03:52:40 +0530

    📦 New component: NavigationBar

--- components/NavigationBar/NavigationBar.js
+++ components/NavigationBar/NavigationBar.js
@@ -1,4 +1,3 @@
-import BulletList from '../../assets/icons/BulletList.svg';
 import Home from '../../assets/icons/MobileBar/Home.svg';
 import School from '../../assets/icons/MobileBar/School.svg';
 import Person from '../../assets/icons/MobileBar/Person.svg';
@@ -7,30 +6,34 @@ import Link from 'next/link';
 
 const svgConfig = {
   height: '30px',
-  width: '30px'
+  width: '30px',
+  fill: 'transparent',
+  stroke: '#000'
 };
 
 const options = [
   {
     logo: <Home {...svgConfig} />,
-    activeLogo: <Home />,
+    activeLogo: <Home {...svgConfig} fill='#000' />,
     text: 'Home',
-    link: '/'
+    link: '/home'
   },
   {
     logo: <School {...svgConfig} />,
-    activeLogo: <BulletList />,
+    activeLogo: <School {...svgConfig} fill='#000' />,
     text: 'Toolbar',
     link: '/toolbar'
   },
   {
     logo: <Person {...svgConfig} />,
-    activeLogo: '😉',
+    activeLogo: <Person {...svgConfig} fill='#000' />,
     text: 'Account',
     link: '/account'
   }
 ];
+
 const NavigationBar = () => {
+  const router = useRouter();
   return (
     <div className='sticky bottom-0 left-0 right-0 w-full max-w-screen-sm py-2 bg-white shadow-above'>
       <div className='flex items-center justify-around gap-2'>
@@ -38,8 +41,17 @@ const NavigationBar = () => {
           return (
             <Link href={item.link} key={idx}>
               <div className='flex flex-col items-center flex-1 pt-1 pb-1 border-r-2 cursor-pointer border-gray-300-500'>
-                <p> {item.logo} </p>
-                <p> {item.text} </p>
+                {router.pathname.includes(item.link) ? (
+                  <>
+                    <p> {item.activeLogo} </p>
+                    <p className='font-semibold'> {item.text} </p>
+                  </>
+                ) : (
+                  <>
+                    <p> {item.logo} </p>
+                    <p className='font-normal'> {item.text} </p>
+                  </>
+                )}
               </div>
             </Link>
           );
