2efa4af822420094e36d2f186b38997af325c7c1
Omeir <omeirf.02@gmail.com>
2021-12-26 02:16:37 +0530

    🎨 ErrorPage: Improve appearance

--- pages/[...404].js
+++ pages/[...404].js
@@ -2,11 +2,13 @@ import ComingSoonIllustration from '../assets/images/coming_soon.svg';
 import Link from 'next/link';
 const ErrorPage = () => {
   return (
-    <div className='flex flex-col items-center p-32'>
-      <p className='text-4xl '>Page not found</p>
+    <div className='flex flex-col items-center p-8 '>
+      <p className='text-4xl text-center '>
+        Oops! <br /> Page not found
+      </p>
       <ComingSoonIllustration className='mt-16' />
       <Link href='account'>
-        <button className='max-w-xs px-4 py-2 bg-purple-500 rounded-lg shadow-lg mt-14 shadow-purple-400'>
+        <button className='max-w-xs px-4 py-2 text-white bg-purple-500 rounded-lg shadow-lg mt-14 shadow-purple-400'>
           Go to Home
         </button>
       </Link>
