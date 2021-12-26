77d25544b988e25332f02c342de661a126c85b18
Omeir <omeirf.02@gmail.com>
2021-12-26 23:28:04 +0530

    📄 New page: /account/contribute

---
+++ pages/account/settings.jsx
@@ -0,0 +1,40 @@
+// Components
+import Header from 'components/Header';
+
+const Settings = () => {
+  return (
+    <div>
+      <Header title='Settings' />
+
+      <main className='p-4'>
+        <p className='text-xl text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique dignissimos
+          eligendi cum, deleniti optio sapiente cupiditate tempore neque esse molestiae! Id libero
+          rerum fugiat, dolor aperiam quasi optio harum?
+        </p>
+        <p className='mt-8 space-y-4 text-xl text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique dignissimos
+          eligendi cum, deleniti optio sapiente cupiditate tempore neque esse molestiae! Id libero
+          rerum fugiat, dolor aperiam quasi optio harum?
+        </p>
+        <p className='mt-8 text-xl text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique dignissimos
+          eligendi cum, deleniti optio sapiente cupiditate tempore neque esse molestiae! Id libero
+          rerum fugiat, dolor aperiam quasi optio harum?
+        </p>
+        <p className='mt-8 text-xl text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique dignissimos
+          eligendi cum, deleniti optio sapiente cupiditate tempore neque esse molestiae! Id libero
+          rerum fugiat, dolor aperiam quasi optio harum?
+        </p>
+        <p className='mt-8 text-xl text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique dignissimos
+          eligendi cum, deleniti optio sapiente cupiditate tempore neque esse molestiae! Id libero
+          rerum fugiat, dolor aperiam quasi optio harum?
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Settings;
