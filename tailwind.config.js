8dd9929c1f0c61efcb7dea160bf1fff7b121cd30
Omeir <omeirf.02@gmail.com>
2021-12-26 21:31:36 +0530

    ⚙ Configure custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   },
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif']
+      },
       boxShadow: {
         above: '2px -8px 20px -10px rgba(0,0,0,0.3)',
         below: '2px 8px 20px -10px rgba(0,0,0,0.3)'
