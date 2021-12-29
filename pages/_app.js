b2c0e4f0ebc54d48b8749bac2adfbd375f290924
Omeir <omeirf.02@gmail.com>
2021-12-30 02:46:13 +0530

    ⚙ Add logos and manifest.json

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,14 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Sched</title>
         <meta name='description' content='An app to keep your university schedule on track' />
-        <link rel='icon' href='/favicon.ico' />
+
+        <link rel='icon' href='/Logo.svg' />
+
+        {/* Link manifest.json */}
+        <link rel='manifest' href='/manifest.json' />
+
+        {/* Set color of URL bar */}
+        <meta name='theme-color' content='#3490DE' />
       </Head>
       <div className=' sm:bg-cyan-900'>
         <div className='max-w-screen-sm sm:mx-auto sm:my-auto sm:bg-white sm:min-h-screen sm:shadow-md sm:shadow-white'>
