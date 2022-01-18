09ccba9808e2b108912d4d3ce84665134a51a112
Omeir <omeirf.02@gmail.com>
2022-01-18 15:09:08 +0530

    📦 conditional render RowItem (if 'title' exists)

--- components/RowItem.jsx
+++ components/RowItem.jsx
@@ -3,26 +3,29 @@ import ChevronRight from '../assets/icons/Account/ChevronRight.svg';
 import { Tag } from './Tags';
 const RowItem = ({ icon, title, tags, description, link, handleClick }) => {
   const router = useRouter();
-  return (
-    <div
-      className='flex items-center py-4 border-b-2 cursor-pointer border-b-gray-100'
-      onClick={handleClick ? () => handleClick() : () => router.push(link)}
-    >
-      <span>{icon}</span>
-      <div className='ml-4'>
-        <span className='font-medium text-gray-600 text-md font-heading'>{title}</span>
-        {tags?.map((tag, idx) => {
-          return (
-            <span key={idx}>
-              <Tag content={tag.content} colortheme={tag.colortheme} />
-            </span>
-          );
-        })}
-        <h6 className='text-sm font-normal text-gray-500'>{description}</h6>
+  if (title)
+    return (
+      <div
+        className='flex items-center py-4 border-b-2 cursor-pointer border-b-gray-100'
+        onClick={handleClick ? () => handleClick() : () => router.push(link)}
+      >
+        <span>{icon}</span>
+        <div className='ml-4'>
+          <span className='font-medium text-gray-600 text-md font-heading'>{title}</span>
+          {tags?.map((tag, idx) => {
+            return (
+              <span key={idx}>
+                <Tag content={tag.content} colortheme={tag.colortheme} />
+              </span>
+            );
+          })}
+          <h6 className='text-sm font-normal text-gray-500'>{description}</h6>
+        </div>
+        <ChevronRight className='ml-auto' fill='#cecece' height='32px' width='32px' />
       </div>
-      <ChevronRight className='ml-auto' fill='#cecece' height='32px' width='32px' />
-    </div>
-  );
+    );
+
+  return null;
 };
 
 export default RowItem;
