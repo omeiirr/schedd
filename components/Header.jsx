9bf619c9b647c6ddb83a1e7e4109d079368662a1
Omeir <omeirf.02@gmail.com>
2022-01-20 17:19:50 +0530

    🎨 conditional shadow on <Header /> acc. to scroll

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import BackArrow from 'assets/icons/BackArrow.svg';
 const Header = ({ title = 'Go back' }) => {
   const router = useRouter();
+
+  const [headerShadow, setHeaderShadow] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 30) setHeaderShadow(true);
+      else setHeaderShadow(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className='sticky top-0 left-0 right-0 w-full bg-white h-18 shadow-below'>
+    <header
+      className={`sticky top-0 left-0 right-0 w-full transition  duration-300 bg-white h-18 
+    ${headerShadow ? 'shadow-below' : ''} `}
+    >
       <div className='flex items-center gap-4 p-4 w-fit' onClick={() => router.back()}>
         <BackArrow width='28px' height='28px' fill='#000' className='cursor-pointer' />
         <p className='text-lg font-medium cursor-pointer font-heading'>{title} </p>
