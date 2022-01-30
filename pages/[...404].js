c5e83837729924b4250dc4a3357032519e2cdbaf
Omeir <omeirf.02@gmail.com>
2022-01-30 19:28:11 +0530

    📄 404 page: UX improvement
    replace 'Go to Home' by 'Go Back'

--- pages/[...404].js
+++ pages/[...404].js
@@ -1,17 +1,20 @@
 import ComingSoonIllustration from '../assets/images/coming_soon.svg';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 const ErrorPage = () => {
+  const router = useRouter();
+
   return (
     <div className='flex flex-col items-center p-8 '>
       <p className='text-4xl text-center '>
         Oops! <br /> Page not found
       </p>
       <ComingSoonIllustration className='mt-16' />
-      <Link href='account' passHref>
-        <button className='max-w-xs px-4 py-2 text-white bg-primary-500 rounded-lg shadow-lg mt-14 shadow-primary-400'>
-          Go to Home
-        </button>
-      </Link>
+      <button
+        className='max-w-xs px-4 py-2 text-white rounded-lg shadow-lg bg-primary-500 mt-14 shadow-primary-400'
+        onClick={() => router.back()}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
