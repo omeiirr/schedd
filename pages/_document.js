0e4d10a5f659e00944dcc368fc67934ee2338498
Omeir <omeirf.02@gmail.com>
2022-01-30 21:32:18 +0530

    ⚙ add Twitter card, update meta description

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta name='description' content='An app to keep your university schedule on track' />
+          <meta
+            name='description'
+            content='Check your Amizone classes, attendance, grades, and more, without the hassle.'
+          />
 
           <link rel='icon' href='/Logo.svg' />
 
@@ -14,6 +17,16 @@ class MyDocument extends Document {
 
           {/* Set color of URL bar */}
           <meta name='theme-color' content='#3490DE' />
+
+          {/* Twitter Card */}
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:site' content='@omeiirr' />
+          <meta name='twitter:title' content='Schedd' />
+          <meta
+            name='twitter:description'
+            content='Check your Amizone classes, attendance, grades, and more, without the hassle.'
+          />
+          <meta name='twitter:image' content='https://www.schedd.xyz/Logo.svg' />
         </Head>
         <body>
           <Main />
