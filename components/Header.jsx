1d71f774db9c9c8412271cfea0f04fdfac2ee8c4
Omeir <omeirf.02@gmail.com>
2022-01-31 22:17:59 +0530

    📦 Header: add prop 'goBackTo'

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import BackArrow from 'assets/icons/BackArrow.svg';
-const Header = ({ title = 'Go back' }) => {
+const Header = ({ title = 'Go back', goBackTo }) => {
   const router = useRouter();
 
   const [headerShadow, setHeaderShadow] = useState(false);
@@ -21,7 +21,13 @@ const Header = ({ title = 'Go back' }) => {
       className={`sticky top-0 left-0 right-0 w-full transition  duration-300 bg-white h-18 
     ${headerShadow ? 'shadow-below' : ''} `}
     >
-      <div className='flex items-center gap-4 p-4 w-fit' onClick={() => router.back()}>
+      <div
+        className='flex items-center gap-4 p-4 w-fit'
+        onClick={() => {
+          if (goBackTo) router.push(goBackTo);
+          else router.back();
+        }}
+      >
         <BackArrow width='28px' height='28px' fill='#000' className='cursor-pointer' />
         <p className='text-lg font-medium cursor-pointer font-heading'>{title} </p>
       </div>
